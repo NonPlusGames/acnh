ff2f8a22c2214d93d3f0be08a66a54bfd158f2b0
Replit user <>
2023-05-12 22:05:58 -0400

    ACNH NETLIFY

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
-# ---- YOUR APP STARTS HERE ----
-
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+import requests
+import json
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
 # -- Routes section --
-
-# INDEX
 @app.route('/')
-@app.route('/index')
 def index():
-  return "hello world"
+  return render_template('index.html')
+  
+@app.route('/quiz', methods=['POST'])
+def quiz():
+  villagers= []
+  response = requests.get("http://acnhapi.com/v1/villagers/")
+  data = response.json()
+  with open("data.json", "w") as f:
+    json.dump(data, f)
+  birthday = request.form['bday']
+  animal = request.form['animal']
+  personality = request.form['personality']
+  similarity=0
+  image=""
+  for key, value in data.items():
+    if birthday.strip().lower() == value["birthday"].strip().lower():
+      similarity= similarity +1
+    if animal.strip().lower() == value["species"].strip().lower():
+      similarity= similarity +1
+    if personality.strip().lower() == value["personality"].strip().lower():
+      similarity= similarity +1
+    if similarity >= 3:
+      villagers.append({"id":value["id"],'name': value["name"]["name-USen"], 'similarity': similarity})
+      image=value["image_uri"]
+  if villagers:
+    villagers.sort(reverse=True, key=lambda e: e["similarity"])
+    villager = villagers[0]["name"]
+  else:
+    villager = "No match found"
+  return render_template('result.html', villager=villager, image=image)
 
-
-
-
-
+    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000)
